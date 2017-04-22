928730db6c2f601cd00f3f93cbd12b1a688a7747
Victor Hugo <victorhundo@gmail.com>
2017-04-22 10:02:19 -0300

    Aldonita enigas mysql kodo

---
+++ uea-mysql/insert.sql
@@ -0,0 +1,7 @@
+use uea;
+
+/* Uzanto */
+INSERT INTO lando () VALUES (1, "nomoLoka", "NomoEo", "landkodo");
+INSERT INTO urbo () VALUES (1, "nomoLoka", "NomoEo", 1);
+INSERT INTO uzantoAuxAsocio (id, ueakodo) VALUES(1,"1");
+INSERT INTO uzanto () VALUES (1, "Victor", "de Sousa",NULL,NULL,"Papa pio X","sei la",1,1,'1993-3-18',NULL,NULL,"notoj","profesio","retposxto", "telhejmo", "teloficejo", "telportebla", "tttpagxo", 1, "abc");

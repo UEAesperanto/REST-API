a8312b21eb1a12710dfacaf9a455de6345800f83
Victor Hugo <victorhundo@gmail.com>
2017-05-13 14:40:20 -0300

    Enigi unu asocio en kodo enigi.sql

--- uea-mysql/enigi.sql
+++ uea-mysql/enigi.sql
@@ -49,6 +49,14 @@ INSERT INTO uzantoAuxAsocio ()
     NULL /* pasvorto */
   );
 
+INSERT INTO uzantoAuxAsocio ()
+  VALUES (
+    3, /* id */
+    "3", /* ueakodo */
+    NULL, /* uzantnomo */
+    NULL /* pasvorto */
+  );
+
 /* --------------- Uzanto ---------------------*/
 INSERT INTO uzanto ()
   VALUES (
@@ -108,4 +116,24 @@ INSERT INTO uzanto ()
     "tttpagxo", /* tttpagxo  */
     1, /* validaKonto  */
     "abc" /* abc  */
+  );
+
+/* ------------ Asocio -------------------- */
+
+INSERT INTO asocio ()
+    VALUES(
+      3, /*id int(11) PRIMARY KEY REFERENCES uzantoAuxAsocio(id)*/
+      "Brazila Esperanto-Ligo", /*nomo varchar(255) */
+      "BEL", /*siglo varchar(255) */
+      "SDS Bl.P 36, Ed. Venâncio III Sala 303, 70393-902 Brasília - DF, Brazilo", /*adreso varchar(255) */
+      "1907-1-1", /* fondigxdato date */
+      "033", /*posxtkodo varchar(255)*/
+      1, /*urbo int(11) NULL REFERENCES urbo(id)*/
+      "(61) 3226 1298", /*telhejmo varchar(255) */
+      25, /* landokodo varchar(255) */
+      "bel@esperanto.org.br", /*retposxto varchar(255), */
+      "delegFako", /* delegFako varchar(255) */
+      "http://www.esperanto.org.br", /* tttpagxo varchar(255) */
+      1, /* junulara boolean - Ĉu tiu asocio estas junulara?*/
+      "abc" /*abc varchar(255)*/
   );

b2f7b1300a67de3f35c90f4d093b0307312ff58c
Victor Hugo <victorhundo@gmail.com>
2017-05-14 11:11:11 -0300

    Enigi unu jurnala asocio en kodo enigi.sql

--- uea-mysql/enigi.sql
+++ uea-mysql/enigi.sql
@@ -57,6 +57,14 @@ INSERT INTO uzantoAuxAsocio ()
     NULL /* pasvorto */
   );
 
+INSERT INTO uzantoAuxAsocio ()
+  VALUES (
+    4, /* id */
+    "4", /* ueakodo */
+    NULL, /* uzantnomo */
+    NULL /* pasvorto */
+  );
+
 /* --------------- Uzanto ---------------------*/
 INSERT INTO uzanto ()
   VALUES (
@@ -134,6 +142,24 @@ INSERT INTO asocio ()
       "bel@esperanto.org.br", /*retposxto varchar(255), */
       "delegFako", /* delegFako varchar(255) */
       "http://www.esperanto.org.br", /* tttpagxo varchar(255) */
+      0, /* junulara boolean - Ĉu tiu asocio estas junulara?*/
+      "abc" /*abc varchar(255)*/
+  );
+
+INSERT INTO asocio ()
+    VALUES(
+      4, /*id int(11) PRIMARY KEY REFERENCES uzantoAuxAsocio(id)*/
+      "Brazila Esperantista Junulara Organizo ", /*nomo varchar(255) */
+      "BEJO", /*siglo varchar(255) */
+      "SDS Bl.P 36, Ed. Venâncio III Sala 303, 70393-902 Brasília - DF, Brazilo", /*adreso varchar(255) */
+      "1907-1-1", /* fondigxdato date */
+      "033", /*posxtkodo varchar(255)*/
+      1, /*urbo int(11) NULL REFERENCES urbo(id)*/
+      "(61) 3226 1298", /*telhejmo varchar(255) */
+      25, /* landokodo varchar(255) */
+      "bel@esperanto.org.br", /*retposxto varchar(255), */
+      "delegFako", /* delegFako varchar(255) */
+      "http://bejo.esperanto.org.br/", /* tttpagxo varchar(255) */
       1, /* junulara boolean - Ĉu tiu asocio estas junulara?*/
       "abc" /*abc varchar(255)*/
   );

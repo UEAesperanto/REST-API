337830237ec45a009e7cfbc4f7cd45938a380b2f
Ana Ribeiro <aninhacostaribeiro@gmail.com>
2017-05-23 16:47:32 -0300

    aldonita enigi skriptoj

--- uea-mysql/enigoj/asocio.sql
+++ uea-mysql/enigoj/asocio.sql
@@ -3,6 +3,28 @@
 
 INSERT INTO asocio ()
     VALUES(
+      1, /*id int(11) PRIMARY KEY REFERENCES uzantoAuxAsocio(id)*/
+      "Universala Esperanto-Asocio", /*nomo varchar(255) */
+      "UEA", /*siglo varchar(255) */
+      "SDS Bl.P 36, Ed. Venâncio III Sala 303, 70393-902 Brasília - DF, Brazilo", /*adreso varchar(255) */
+      "1907-1-1", /* fondigxdato date */
+      "033", /*posxtkodo varchar(255)*/
+      1, /*urbo int(11) NULL REFERENCES urbo(id)*/
+      NULL, /*fako*/
+      25, /* landokodo varchar(255) */
+      "(61) 3226 1298", /*telhejmo varchar(255) */
+      "bel@esperanto.org.br", /*retposxto varchar(255), */
+      "delegFako", /* delegFako varchar(255) */
+      "http://www.esperanto.org.br", /* tttpagxo varchar(255) */
+      0, /* junulara boolean - Ĉu tiu asocio estas junulara?*/
+      0, /*Ĉu tiu asocio estas faka?*/
+      1, /*Ĉu tiu asocio estas landa?*/
+      "abc" /*abc varchar(255)*/
+  );
+
+
+INSERT INTO asocio ()
+    VALUES(
       3, /*id int(11) PRIMARY KEY REFERENCES uzantoAuxAsocio(id)*/
       "Brazila Esperanto-Ligo", /*nomo varchar(255) */
       "BEL", /*siglo varchar(255) */
---
+++ uea-mysql/enigoj/grupa-kategorio.sql
@@ -0,0 +1,6 @@
+/*ekzemple, membro, komisiono, ktp*/
+INSERT INTO grupa_kategorio ()
+  VALUES (
+    1,  /*id laborgrupo*/
+    "laboro" /*nomo varchar(255)*/
+  );

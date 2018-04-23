5abec62de6d1f79933917c8a0dff49effc5b2ebb
victorhundo <victorhundo@gmail.com>
2018-04-22 22:14:37 -0300

    Ĝisdatigita Dockerfiles kaj docker-compose arkivoj

--- docker/dockerfiles/node-dev/entrypoint.sh
+++ docker/dockerfiles/node-dev/entrypoint.sh
@@ -7,12 +7,16 @@ while ping -q -c1 $LIQUIBASE > /dev/null; do
 done
 
 echo "app preta!"
-if [ -z "$TEST" ]; then
-  npm install --unsafe-perm --dev && nodemon
-else
-  npm install --unsafe-perm --dev
-  nodemon \
-    --ext js \
-    --watch ./ --exec 'mocha ./test || true' \
-    --delay 1
-fi
+npm install --unsafe-perm --only=dev
+node --inspect=0.0.0.0:9229 server
+tail -f /dev/null
+ 
+# if [ -z "$TEST" ]; then
+#   npm install --unsafe-perm --dev && nodemon
+# else
+#   npm install --unsafe-perm --dev
+#   nodemon \
+#     --ext js \
+#     --watch ./ --exec 'mocha ./test || true' \
+#     --delay 1
+# fi

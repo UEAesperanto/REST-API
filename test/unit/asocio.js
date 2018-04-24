cc9ab0d442fdafa1a2768b0d9bd39e11ff996159
victorhundo <victorhundo@gmail.com>
2018-04-24 16:45:37 -0300

    Aldonita unit test al asocio

---
+++ test/unit/asocio.js
@@ -0,0 +1,22 @@
+describe('ASOCIO', () => {
+
+  //Before each test we empty the database
+  beforeEach((done) => {
+      createAdmin();
+      cleanTable('asocio');
+      token = generateToken();
+      done();
+  });
+
+  describe('GET /asocioj', () => {
+    it('it should GET all the asocioj',(done) => {
+      request
+        .get('/asocioj')
+        .end((err,res) => {
+          res.status.should.be.equal(200);
+          done();
+        });
+    });
+  });
+
+});

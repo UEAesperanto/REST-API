87b9bf2fb04868a1437b6af440d4046182d7991a
Ana Ribeiro <aninhacostaribeiro@gmail.com>
2018-04-26 03:11:42 +0200

    Aldonita Admin

--- modules/auth0.js
+++ modules/auth0.js
@@ -5,12 +5,9 @@
  */
 module.exports = function handleCallbackError() {
   return function(req, res, next) {
-    console.log(req);
-    console.log("in between")
-    console.log('req.query.err',req.query.error);
-    if (req.query.error){
+    if ((req.query) && (req.query.error)){
       return next(new Error(req.query.error +': '+req.query.error_description));
     }
     next();
-};
+  }
 };

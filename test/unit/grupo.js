9885d8e26a96d3071714f52b84a928a2def3ba53
victorhundo <victorhundo@gmail.com>
2018-04-27 19:24:17 -0300

    Aldonia POST /grupoj/kategorioj

--- test/unit/grupo.js
+++ test/unit/grupo.js
@@ -1,6 +1,7 @@
 describe('==== GRUPO ====', () => {
   token = '';
   grupoModel1 = {mallongigilo: 'mallongigilo' , nomo: 'nomo', priskribo: 'priskribo'};
+  kategoriojModel1 = {nomo: 'kategorio1'};
 
   //Before each test we empty the database
   beforeEach((done) => {
@@ -57,6 +58,59 @@ describe('==== GRUPO ====', () => {
               res.body[0].priskribo.should.equal(grupoModel1.priskribo);
               done();
             });
+        });
+    });
+  });
+
+  describe('POST /grupoj/kategorioj', () => {
+    it('it should POST a kategorio', (done) => {
+      request
+        .post('/grupoj/kategorioj')
+        .send(kategoriojModel1)
+        .set('x-access-token', token)
+        .end((err,res) => {
+          res.status.should.be.equal(201);
+          done();
+        });
+    });
+  });
+
+  describe('GET /grupoj/kategorioj/:id/sub', () => {
+    it('it should GET all the grupoj/laboroj with body', (done) => {
+      request
+        .get('/grupoj/kategorioj/1/sub')
+        .end((err,res) => {
+          res.status.should.be.equal(200);
+          done();
+        });
+    });
+
+    it('it should GET all the grupoj/membrecoj with body', () => {
+      request
+        .get('/grupoj/kategorioj/4/sub')
+        .end((err,res) => {
+          res.status.should.be.equal(200);
+          done();
+        });
+    });
+
+    it('it should GET all the aldonaj membrecoj with body', () => {
+      request
+        .get('/grupoj/kategorioj/5/sub')
+        .end((err,res) => {
+          res.status.should.be.equal(200);
+          done();
+        });
+    });
+  });
+
+  describe('GET /grupoj/1/kotizoj', () => {
+    it('', (done) => {
+      request
+        .get('/grupoj/1/kotizoj')
+        .end((err,res) => {
+          res.status.should.be.equal(200);
+          done();
         });
     });
   });

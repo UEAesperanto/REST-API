65454d4a2e7ce882e41db2a6484241c06d06e746
Robin van der Vliet <info@robinvandervliet.com>
2018-10-16 22:20:26 +0200

    Update configMail.js

--- configMail.js
+++ configMail.js
@@ -1,41 +1,41 @@
 module.exports = {
   'registriUzanton':    'Estimata %s, <br><br>\
                         Via aliĝo por UEA estis registrita. En kelkaj tagoj, vi ricevos konfirmon\
-                        de via pago kaj povos ekuzi viajn membrservojn<br>\
-                        En kazo de duboj, kontaktu info@uea.org. \
+                        de via pago kaj povos ekuzi viajn membroservojn<br>\
+                        En okazo de duboj, kontaktu info@uea.org. \
                         <br><br>Kore,<br><br>\
                         La UEA-Teamo' ,
   'novaPasvorto':       'Estimata uzanto, <br><br>\
-                        La pasvorto por via membrspaco ĉe UEA estas nun: %s  <br> \
+                        La pasvorto por via membrospaco ĉe UEA estas nun: %s  <br> \
                         Ni rekomendas tuj ŝanĝi tiun pasvorton je ensaluto en la membra retejo. \
                         <br><br>Agrablan uzadon,<br><br>\
                         La UEA-Teamo',
   'updateUzanto':       'Estimata %s, <br><br>\
                         Iu petis por ĝisdatigi vian %sn al %s.  \
-                        Se tio ŝajnas enordo, ne estas bezonata fari ion ajn. \
-                        Se tio ne estas enordo, bonvole kontaktu "sekretario@co.uea.org"\
+                        Se tio estas en ordo, ne necesas fari ion ajn. \
+                        Se tio ne estas en ordo, kontaktu "sekretario@co.uea.org"\
                         <br><br>Kore,<br><br>\
                         La UEA-Teamo',
   'membrecAprobita':    'Estimata %s, <br><br>\
                         Via peto por membriĝi en %s en UEA/TEJO estis akceptita. \
-                        En kazo de duboj, kontaktu info@uea.org. \
+                        En okazo de duboj, kontaktu info@uea.org. \
                         <br><br>Kore,<br><br>\
                         La UEA-Teamo',
   'membrecMalaprobita': 'Estimata %s, <br><br>\
                         Via peto por membriĝi en %s en UEA/TEJO estis malaprobita. \
                         Plej verŝajne temas pri pag-problemo.\
-                        En kazo de duboj, kontaktu info@uea.org. \
+                        En okazo de duboj, kontaktu info@uea.org. \
                         <br><br>Kore,<br><br>\
                         La UEA-Teamo',
   'membrecRenovigita':  'Estimata %s, <br><br>\
                         Via membrectempo por %s en UEA/TEJO estis renovigita, \
-                        nun ĝi validas ĝis %s .\
-                        En kazo de duboj, kontaktu info@uea.org. \
+                        nun ĝi validas ĝis %s.\
+                        En okazo de duboj, kontaktu info@uea.org. \
                         <br><br>Kore,<br><br>\
                         La UEA-Teamo',
   'membrecVisxita':     'Estimata %s, <br><br>\
                         Via membreco en %s estis viŝita. \
-                        En kazo de duboj, kontaktu sekretario@co.uea.org. \
+                        En okazo de duboj, kontaktu sekretario@co.uea.org. \
                         <br><br>Kore,<br><br>\
                         La UEA-Teamo'
 };
